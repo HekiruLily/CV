91867cb511a89170492552908d8290dc574a5994
Nguyễn Đức Thành <165492722+thanhanmuop1@users.noreply.github.com>
2025-02-24 03:14:17 +0700

    Update upload.js

--- backend/src/middlewares/upload.js
+++ backend/src/middlewares/upload.js
@@ -5,7 +5,11 @@ const fs = require('fs');
 // Cấu hình nơi lưu trữ và tên file
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads');
+        const uploadDir = req.body.uploadDir || 'uploads/';
+        if (!fs.existsSync(uploadDir)) {
+            fs.mkdirSync(uploadDir, { recursive: true });
+        }
+        cb(null, uploadDir);
     },
     filename: (req, file, cb) => {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
