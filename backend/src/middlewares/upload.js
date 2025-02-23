8c0109d88d6929251d474843282183e2c201bc53
giacatdat2k5 <141319331+giacatdat2k5@users.noreply.github.com>
2025-02-24 03:03:37 +0700

    UpdateProfile

--- backend/src/middlewares/upload.js
+++ backend/src/middlewares/upload.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 // Cấu hình nơi lưu trữ và tên file
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads/club_avatars/');
+        cb(null, 'uploads');
     },
     filename: (req, file, cb) => {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
