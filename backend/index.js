3d087c1bded49509f2688b6e3539d7586e539091
thanhanmuop1 <thanhnd.b23cc152@stu.ptit.edu.vn>
2025-03-01 03:00:55 +0700

    Update server port configuration to use environment variable

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const port = 5000;
 const cors = require('cors');
 const clubRoutes = require('./src/routes/club.routes');
 
@@ -15,6 +14,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/clubs', clubRoutes);
 
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
 });
